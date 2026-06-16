2a7cbbd16620a59fd4acd4dcfa2b35a20c216468
Ubuntu <ubuntu@ip-172-31-15-226.ap-south-2.compute.internal>
2026-06-16 05:08:11 +0000

    Update message v2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from my CI/CD Pipeline!";
+        return "Version 2 - Auto Deployed by CI/CD!";
     }
 }
